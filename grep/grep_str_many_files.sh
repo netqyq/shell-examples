ed160fccb7f27f51f1091f4ba364ff1714178ecf
Chaniel <Chaniel@users.noreply.github.com>
2015-05-29 07:39:04 +0800

    Update grep_str_many_files.sh

--- grep/grep_str_many_files.sh
+++ grep/grep_str_many_files.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-# Purpose: find string "POSIX" in many C source files.
 
+
+# case 1, Purpose: find string "POSIX" in many C source files, and print the matched file name and file content.
 for file in *
 do
   if grep -l POSIX $file
@@ -8,3 +9,10 @@ do
     more $file
   fi
 done
+
+# one line shell, function equals to above.
+# for file in *; do if grep -l hello $file; then more $file; fi; done
+
+
+
+find ./ -name "*" | xargs grep ""
